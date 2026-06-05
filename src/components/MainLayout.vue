5abcf041fe3e49b0584de2e08ae2912879c0bc40
wesleyfelix <wesley.felixdelima96@hotmail.com>
2026-06-05 14:49:11 -0300

    iniando a estrutura das telas e rotas

---
+++ src/components/MainLayout.vue
@@ -0,0 +1,38 @@
+<template>
+  <q-layout
+      view="lHh lpr lFf"
+      class="full-screen"
+  >
+    <q-header elevated>
+      <q-toolbar>
+        <q-avatar>
+          <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg" />
+        </q-avatar>
+        <q-toolbar-title>
+          Quasar Framework teste
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <slot></slot>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  name: "MainLayout",
+  methods: {
+    logout() {
+      this.$router.push('/');
+    }
+  }
+}
+</script>
+
+<style scoped>
+.full-screen {
+  min-height: 100vh;
+}
+</style>
---
+++ src/views/HomePage.vue
@@ -0,0 +1,24 @@
+<template>
+  <MainLayout>
+    <q-page-container>
+      <q-page class="q-pa-md">
+        <div class="q-pa-md q-gutter-sm">
+          <q-btn color="primary" label="Novo agendamento" icon="request_page"/>
+          <q-btn color="secondary" glossy label="Extrato" icon="text_snippet"/>
+        </div>
+      </q-page>
+    </q-page-container>
+  </MainLayout>
+</template>
+<script>
+import MainLayout from '@/components/MainLayout.vue';
+export default {
+  components: {MainLayout},
+  name: 'HomePage',
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
